986639c72e2559e25a71800001b2c995fd49ffa2
Emerson Pinheiro <emersonline2007@gmail.com>
2019-10-29 11:13:34 -0400

    - Visão geral - Hello World - Instalação via Composer - Fomulários - Banco de Dados - O Famoso Gii - Estrutura de uma Aplicação - A Aplicação

---
+++ basic/config/params.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'adminEmail' => 'admin@example.com',
+    'senderEmail' => 'noreply@example.com',
+    'senderName' => 'Example.com mailer',
+    // 'pagseguro_token' => 'dkjwiejqwoeijqwoe1ij1k1',
+];
---
+++ basic/views/hello/say-something.php
@@ -0,0 +1,4 @@
+<?php
+?>
+
+<h2>O valor da variável $message é: <?= \yii\helpers\Html::encode($message) ?></h2>
